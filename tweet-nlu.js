58ee79d02433a7f9c6c273fccf417d2ecb97ded7
Ben Smith <benwsmith@gmail.com>
2018-03-22 11:34:59 -0600

    refined the output to make it look nicer

--- tweet-nlu.js
+++ tweet-nlu.js
@@ -19,19 +19,31 @@ var parameters = {
 
 twitter.get('/statuses/user_timeline', {
     screen_name: 'realDonaldTrump',
-    count: 1
+    count: 3
 }).then(function (data) {
-    var tweets = data.map(function (tweet) {
+    return tweets = data.map(function (tweet) {
         return tweet.text;
     });
-
-    tweets.forEach(function(tweet, index) {
-        nluAnalyze(tweet).then(function(analysis) {
-            console.log('Tweet: ' + index, JSON.stringify(analysis, null, 2));
+}).then(function() {
+    return tweets.asyncMap(function(tweet, index) {
+        return nluAnalyze(tweet).then(function(analysis) {
+            // console.log('Tweet: ' + index, JSON.stringify(analysis, null, 2));
+            return tweets[index] = {
+                "tweet": tweet,
+                "sentiment": analysis.sentiment.document,
+                "emotion": analysis.emotion.document.emotion,
+                "subject": {
+                    "target": analysis.entities[0].text,
+                    "sentiment": analysis.entities[0].sentiment,
+                    "emotion": analysis.entities[0].emotion
+                }
+            };
         }).catch(function(err) {
             throw JSON.stringify(err, null, 2);
         })
     });
+}).then(function() {
+    console.log(JSON.stringify(tweets, null, 2));
 }).catch(function (err) {
     throw JSON.stringify(err, null, 2);
 });
@@ -49,48 +61,12 @@ function nluAnalyze(tweet) {
     })
 }
 
-/*
-// nlu.analyze(parameters, function (err, resp) {
-//     if (err) {
-//         console.log('nlu error:', err);
-//         return false;
-//     } else {
-//         console.log(JSON.stringify(resp));
-//     }
-// })
-//*/
-// var nluAnalyze = function (tweets) {
-//     tweets.forEach(function (tweet) {
-//         var parameters = {
-//             'text': tweet,
-//             'return_analyzed_text': true,
-//             'features': {
-//                 'emotion': {
-//                 },
-//                 'sentiment': {
-//                 },
-//                 'concepts': {
-//                     'limit': 3
-//                 },
-//                 'entities': {
-//                     'sentiment': true,
-//                     'emotion': true,
-//                     'limit': 3,
-//                 }
-//             }
-//         };
-
-//         nlu.analyze(parameters, nluResult);
-//     })
-// }
-
-// var error = function (err, resp, body) {
-//     console.log('ERROR:', err);
-// }
-
-// var success = function (data) {
-//     var tweets = JSON.parse(data).map(function (tweet) {
-//         return tweet.text;
-//     });
-//     console.log(tweets);
-// }
+Array.prototype.asyncMap = function(callback) {
+    return Promise.resolve(this).then( async function(ar) {
+        var out = [];
+        for(var i = 0; i < ar.length; i++) {
+            out[i] = await callback.call(ar, ar[i], i, ar);
+        }
+        return out;
+    })
+}
