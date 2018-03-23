b84f478482e36b8b54979b9c52922768d4918569
Ben Smith <benwsmith@gmail.com>
2018-03-22 19:22:30 -0600

    add simple expressjs example of usage

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var tweets = require('./tweet-nlu');
+
+app.get('/', function(req, res, next) {
+	tweets().then(function(tweets) {
+		res.send(JSON.stringify(tweets));
+	}).catch(next)
+});
+
+app.use(function(err, req, res, next) {
+	// handle error
+	res.send('error\n' + JSON.stringify(err, null, 2));
+})
+
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!');
+})
